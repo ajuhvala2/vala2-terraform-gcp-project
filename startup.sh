29ea034e40c30fbec84ee4834269ace1aed64c8b
ajuhvala2 <ajuhvala2gmail.com>
2024-04-13 01:11:41 -0400

    update commit

--- startup.sh
+++ startup.sh
@@ -3,7 +3,7 @@ sudo apt update && sudo apt upgrade -y
 sudo apt install nginx -y 
 sudo systemctl enable nginx
 sudo systemctl start nginx
-git clone https://git.toptal.com/screening-ops/vala-ajuh.git
+git clone https://git.eb.com/screening-ops/vala-ajuh.git
 cd /vala-ajuh
 pip install requirements.txt
 echo "*/10 * * * * $SHELL remediation.sh" | crontab -
